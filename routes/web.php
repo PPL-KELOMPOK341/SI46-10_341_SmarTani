869f51293c5fa4ac236e9f2ae11362e103859cd4
rafi muhammad <balurana.baau@gmail.com>
2025-04-08 16:31:33 +0700

    Membuat project laravel

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AuthController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/register', [RegisterController::class, 'showForm'])->name('register');
+// Route::post('/register', [RegisterController::class, 'register']);
+
+
+
+// Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+// Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
