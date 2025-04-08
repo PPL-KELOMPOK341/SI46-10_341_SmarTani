854aef9b3691cfaa276c2b15a474b176ac6089dd
rafi muhammad <balurana.baau@gmail.com>
2025-04-08 17:52:18 +0700

    S13S-2 membuat route login

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Registration
 Route::get('/register', [RegisterController::class, 'showForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
 
-
-
-// Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-// Route::post('/login', [AuthController::class, 'login']);
-// Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+// Login
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
